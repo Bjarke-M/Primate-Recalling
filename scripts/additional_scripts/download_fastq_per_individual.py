62eaea27f7e9d3562d2cfe769bc6fc36dce781c3
Juraj Bergman <jurajbergman@gmail.com>
2025-08-26 09:54:54 +0200

    Update download_fastq_per_individual.py

--- scripts/additional_scripts/download_fastq_per_individual.py
+++ scripts/additional_scripts/download_fastq_per_individual.py
@@ -10,7 +10,7 @@ run_accessions = str(sys.argv[3]).split(",")
 #### run
 attempt = 0
 a_files = [f"download_pe2_{group}_" + run_accession.replace("/", "_") for run_accession in run_accessions]
-a_exist = [f for f in a_files if os.path.isfile(f"/faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/"+f)]
+a_exist = [f for f in a_files if os.path.isfile(f"~/{group}/done/"+f)]
 while len(a_files) != len(a_exist):
     os.system(f"""echo Attempt: "{attempt}"
     """)
@@ -19,25 +19,25 @@ while len(a_files) != len(a_exist):
         run_accession_name = run_accession.split("/")[-1]
         job_name = f"download_pe2_{group}_" + run_accession.replace("/", "_")
 
-        if job_name in os.listdir(f"/faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/"):
+        if job_name in os.listdir(f"~/{group}/done/"):
             os.system(f"""
-            echo "{job_name} present in /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/"
+            echo "{job_name} present in ~/{group}/done/"
             echo "Skipping..."
             """)
         else:
             os.system(f"""
             echo "Starting download: {run_accession_name}"
             
-            rm -f /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/fastq/{run_accession_name}
+            rm -f ~/{group}/fastq/{run_accession_name}
             
-            wget --progress=dot:giga --timeout=120 --waitretry=60 --tries=10000 --retry-connrefused -P /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/fastq/ {run_accession}
+            wget --progress=dot:giga --timeout=120 --waitretry=60 --tries=10000 --retry-connrefused -P ~/{group}/fastq/ {run_accession}
             
             echo "Download succesful: {run_accession_name}"
             
-            touch /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/{job_name}
+            touch ~/{group}/done/{job_name}
             """)
 
-    a_exist = [f for f in a_files if os.path.isfile(f"/faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/"+f)]
+    a_exist = [f for f in a_files if os.path.isfile(f"~/{group}/done/"+f)]
     attempt += 1
     l1 = len(a_files)
     l2 = len(a_exist)
@@ -51,6 +51,6 @@ while len(a_files) != len(a_exist):
 time.sleep(60*15)
 if len(a_files) == len(a_exist):
     os.system(f"""
-              echo "Making master done file: /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/download_per_individual_{group}_{ind}"
-              touch /faststorage/project/primatediversity/data/gVCFs_recalling_10_12_2024/{group}/done/download_per_individual_{group}_{ind}
+              echo "Making master done file: ~/{group}/done/download_per_individual_{group}_{ind}"
+              touch ~/{group}/done/download_per_individual_{group}_{ind}
               """)
